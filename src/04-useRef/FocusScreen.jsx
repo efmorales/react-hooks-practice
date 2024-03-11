00e5e15add94c788b8964a3e2f5fe5537ec33d50
efmorales <54375116+efmorales@users.noreply.github.com>
2024-03-11 16:24:09 -0400

    added FocusScreen.jsx component

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,30 @@
+import { useRef } from 'react';
+
+export const FocusScreen = () => {
+
+    const inputRef = useRef()
+
+    const onClick = () => {
+        console.log(inputRef.current.select())
+        inputRef.current.select()
+    }
+
+
+  return (
+    <>
+        <h1>Focus Screen</h1>
+        <hr />
+        <input
+            ref={inputRef}
+            type="text"
+            className="form-control"
+            placeholder="Your name"
+        />
+
+        <button className="btn btn-outline-primary mt-5"
+        onClick={onClick}>
+            Set focus
+        </button>
+    </>
+  )
+}
