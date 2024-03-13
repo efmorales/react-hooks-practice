747520db496ad9d2792305f3fd2353ab42182000
efmorales <54375116+efmorales@users.noreply.github.com>
2024-03-13 12:08:06 -0400

    added intro-reducer.js file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -10,12 +10,14 @@ import ReactDOM from 'react-dom/client'
 // import { Memorize } from '../06-memos/Memorize'
 // import { MemoHook } from '../06-memos/MemoHook'
 // import { CallbackHook } from '../06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+import { Padre } from './07-tarea-memo/Padre';
+
+import './08-useReducer/intro-reducer';
 
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Padre />
-  // </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   <Padre />
+//   // </React.StrictMode>,
+// )
