c28cec64ccd6b5e7a639e0b752b1e26bbd64d62d
efmorales <54375116+efmorales@users.noreply.github.com>
2024-03-18 10:21:53 -0400

    updated login component with usecontext hook

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,8 +1,30 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+
+    const {user, setUser} = useContext( UserContext );
+
+    console.log(user);
+
     return (
         <>
             <h1>LoginPage</h1>
             <hr />
+
+            <pre>
+                {JSON.stringify(user, null, 3)}
+            </pre>
+
+            <button 
+            className='btn btn-primary'
+            onClick={ () => setUser({
+                id: 123,
+                name: 'Roy Mustang',
+                email: 'roy123@hey.com'
+            })}>
+                Set User
+            </button>
         </>
     )
 }
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-    id: 1,
-    name: "Enzo Morales",
-    email: "enzomc@gmail.com"
-}
+// const user = {
+//     id: 1,
+//     name: "Enzo Morales",
+//     email: "enzomc@gmail.com"
+// }
 
 
 export const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState({});
+
     return (
-        <UserContext.Provider value={{user}}>
+        // <UserContext.Provider value={{user}}>
+        <UserContext.Provider value={{user, setUser}}>
             {children}
         </UserContext.Provider>
     )
